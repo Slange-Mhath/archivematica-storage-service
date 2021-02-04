1b6275fb6f696d2f0475d8c8b23d162f81075b2c
Tessa Walsh <tessa@bitarchivist.net>
2021-02-04 16:05:41 -0500

    Add test docstrings

--- storage_service/common/tests/test_middleware.py
+++ storage_service/common/tests/test_middleware.py
@@ -14,6 +14,7 @@ class AuditLogMiddlewareTestCase(TestCase):
         self.client.force_login(self.user)
 
     def test_audit_log_middleware_adds_username(self):
+        """Test that X-Username is added for authenticated users."""
         with self.modify_settings(
             MIDDLEWARE={"append": "common.middleware.AuditLogMiddleware"}
         ):
@@ -22,9 +23,16 @@ class AuditLogMiddlewareTestCase(TestCase):
             self.assertEqual(response["X-Username"], self.user.username)
 
     def test_audit_log_middleware_unauthenticated(self):
+        """Test absence of X-Username header for unauthenticated users.
+
+        First we logout the authenticated user, and then we check for
+        the presence of X-Username in the response for a new request by
+        an unauthenticated user.
+        """
         with self.modify_settings(
             MIDDLEWARE={"append": "common.middleware.AuditLogMiddleware"}
         ):
             self.client.logout()
+
             response = self.client.get(settings.LOGIN_URL)
             self.assertFalse(response.has_header("X-Username"))
